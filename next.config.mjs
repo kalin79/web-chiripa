7553f705387ab21b0ad3cd1e1373a29bfe06a163
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2024-12-11 18:52:30 -0500

    volviendo a la configuracion normal

--- next.config.mjs
+++ next.config.mjs
@@ -6,52 +6,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const sass = require('sass');
 
-const securityHeaders = [
-    {
-        key: "Content-Security-Policy",
-        value: `
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://trusted-scripts.com https://www.googletagmanager.com https://www.google.com.pe;
-        img-src 'self' data: https://trusted-images.com;
-        style-src 'self' 'unsafe-inline';
-        connect-src 'self';
-        font-src 'self' https://fonts.gstatic.com;
-        frame-src 'self';
-      `.replace(/\n/g, ""), // Elimina los saltos de línea para compatibilidad
-    },
-    {
-        key: "Strict-Transport-Security",
-        value: "max-age=31536000; includeSubDomains; preload",
-    },
-    {
-        key: "X-XSS-Protection",
-        value: "1; mode=block",
-    },
-    {
-        key: "X-Frame-Options",
-        value: "DENY",
-    },
-    {
-        key: "X-Content-Type-Options",
-        value: "nosniff",
-    },
-    {
-        key: "Permissions-Policy",
-        value: "geolocation=(), microphone=(), camera=()",
-    },
-];
-
-
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                // Aplica las cabeceras de seguridad a todas las rutas
-                source: "/(.*)",
-                headers: securityHeaders,
-            },
-        ];
-    },
     sassOptions: {
         implementation: sass,
         sourceMap: true,
