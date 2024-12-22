d05fd0263bb214847a3669f7ee3486bfe44a1115
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2024-12-21 19:22:24 -0500

    proyecto listo

--- src/app/(frontend)/ganadores/page.tsx
+++ src/app/(frontend)/ganadores/page.tsx
@@ -1,4 +1,4 @@
-// import { Suspense } from 'react'
+import { Suspense } from 'react'
 import ListadoSeccion from "@/components/ganadores/Listado"
 import FormularioSuscripcion from "@/components/suscripcion/formulario"
 import { processApi } from '@/actions/form.actions'
@@ -18,24 +18,15 @@ const getData = async (token: any) => {
 
 const Ganadores = async () => {
     let data = null;
-    try {
-        const response = await getData(process.env.NEXT_PUBLIC_AUTHORIZATION_FORM);
-        data = response;
-        console.log(response)
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return <div>Error loading data</div>;
-    }
-
-    if (!data) {
-        return <div>Loading...</div>;
-    }
+    const response = await getData(process.env.NEXT_PUBLIC_AUTHORIZATION_FORM);
+    data = response;
     return (
         <div className={`backgroundContainer`}>
-            {JSON.stringify(data)}
-            <ListadoSeccion dataGanadores={data} />
-            <FormularioSuscripcion />
+            {/* {JSON.stringify(data)} */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <ListadoSeccion dataGanadores={data} />
+                <FormularioSuscripcion />
+            </Suspense>
         </div>
     )
 }
