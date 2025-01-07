0670529afb44c853906b0eb123fd92c1be9c22d0
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2025-01-07 11:55:06 -0500

    menu actualizado

--- src/components/template/MainHeader.tsx
+++ src/components/template/MainHeader.tsx
@@ -182,7 +182,7 @@ const Header = () => {
                                     height={6}
                                 />
                             </Link>
-                            <Link onClick={handleClickClose} href="/listado-de-sorteos" className={`${pathname === '/listado-de-sorteos' ? styles.active : ''}`}>
+                            <Link onClick={handleClickClose} href="/sorteos" className={`${pathname === '/listado-de-sorteos' ? styles.active : ''}`}>
                                 Sorteos
                                 <Image
                                     className={styles.curvaNav}
